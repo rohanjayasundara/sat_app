9bcf7332b7822b3c6878c0c759c4ff702b914ce7
Rohan <rohanjayasundara89@gmail.com>
2021-05-14 14:22:14 +0530

    New app

---
+++ ifs_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IfsAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ifs_app'
--- sat_app/settings.py
+++ sat_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ifs_app',
+    'fast_react',
 ]
 
 MIDDLEWARE = [
